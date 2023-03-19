80f1ab3ba52bcc9637fd0d2d0a22659d861a47b1
James Murdza <james@jamesmurdza.com>
2023-03-19 20:19:23 +0100

    Improve prompt for concision and reliability.

--- prompt.js
+++ prompt.js
@@ -1,20 +1,20 @@
 const template = `
-Help me to make: 
+Help me to create a repository containing the code for: 
 - {DESCRIPTION}
 
 The project name is: {REPOSITORY_NAME}
 
-Give me instructions to do this, but use only shell commands. No need to give further explanation. All of your output should be given as a single bash script.
+Give me instructions to generate the code for this project, but use only shell commands. No need to give further explanation. All of your output should be given as a single /bin/sh script.
 
-For example, when want to edit a file, use the command \`echo "file contents" > filename.ext\`.
+For example, when want to edit a file, use the command \`echo "file contents" > filename.ext\`. Create intermediate directories before writing files.
 
 At the beginning of the script, create a git repo before running any other commands. Then, create a .gitignore file.
 
-Assume all commands will be run on a clean \`{BASE_IMAGE}\` system with no packages installed. If you need to install a package, use the appropriate command such as \`apt-get install\`, \`npm install\`, etc.
+Assume all commands will be run on a clean \`{BASE_IMAGE}\` system with no packages installed.
 
 Follow each command in the instructions with a git commit command including a detailed commit message. Follow each file change in the instructions with a git commit command including a detailed commit message.
 
-Do not include any commands to run or start the app. Do not include any commands to test the app.
+Do not include any commands to run, start or deploy or test the app.
 
 Do not use an exit command at the end of the script.
 `;
