7b631c5d52ab5129aa89c70651453bf1b6fc62fe
James Murdza <james@jamesmurdza.com>
2023-04-04 20:35:50 +0100

    Change replaceAll() to replace() to support earlier ECMAScript versions.

--- corrections.ts
+++ corrections.ts
@@ -1,13 +1,13 @@
 // Post-processing:
 
 function applyCorrections(buildScript: string) {
-    // What a hack! But small corrections like this will necessary for a while.
-    return buildScript.replaceAll(/^npx /mg, 'npx --yes ')
-        .replaceAll(/^echo /mg, 'echo -e ')
-        .replaceAll(/^git push .*$/mg, '')
-        .replaceAll(/^git remote .*$/mg, '')
-        .replaceAll(/^git commit -m "(.*)$/mg, 'git commit -m "👷🏼 $1')
-        .replaceAll(/^git commit (.*)$/mg, 'git commit -a $1');
+  // What a hack! But small corrections like this will necessary for a while.
+  return buildScript.replace(/^npx /mg, 'npx --yes ')
+    .replace(/^echo /mg, 'echo -e ')
+    .replace(/^git push .*$/mg, '')
+    .replace(/^git remote .*$/mg, '')
+    .replace(/^git commit -m "(.*)$/mg, 'git commit -m "👷🏼 $1')
+    .replace(/^git commit (.*)$/mg, 'git commit -a $1');
 }
 
 export { applyCorrections };
