570e80602633b1b204b6e2e4619618c5b5267afb
James Murdza <james@jamesmurdza.com>
2023-04-06 09:05:53 +0100

    Use cached OpenAI responses when enabled.

--- openai.ts
+++ openai.ts
@@ -4,9 +4,17 @@ import { Configuration, OpenAIApi } from 'openai'
 
 async function simpleOpenAIRequest(prompt: string, config: any) {
 
+  const baseOptions = process.env.OPENAI_CACHE_ENABLED ? {
+    headers: {
+      "Helicone-Cache-Enabled": "true",
+      "Helicone-Cache-Bucket-Max-Size": "1"
+    },
+  } : {};
+
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
     basePath: process.env.OPENAI_BASE_URL,
+    baseOptions: baseOptions,
   })
   const openai = new OpenAIApi(configuration)
 
