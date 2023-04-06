335f852bb86a08c9bd657499ca280efc27f3478f
James Murdza <james@jamesmurdza.com>
2023-04-06 14:26:37 +0100

    Correct the build script by adding mkdir commands where needed.

--- corrections.ts
+++ corrections.ts
@@ -1,9 +1,14 @@
 // Post-processing:
 
 function applyCorrections(buildScript: string) {
+
+  // Detect commands like: echo -e "..." > ...
+  const echoRedirection = /^((?<COMMAND>echo -e "(?:\\.|[^\\"])*") > (?<DIRECTORY>.*\/).*)$/mg;
+
   // What a hack! But small corrections like this will necessary for a while.
   return buildScript.replace(/^npx /mg, 'npx --yes ')
     .replace(/^echo /mg, 'echo -e ')
+    .replace(echoRedirection, 'mkdir -p $<DIRECTORY> && $1')
     .replace(/^git push .*$/mg, '')
     .replace(/^git remote .*$/mg, '')
     .replace(/^git commit -m "(.*)$/mg, 'git commit -m "👷🏼 $1')
