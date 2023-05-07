8316f43aecb694ddd06a657f142d7c1d4ff4b061
James Murdza <james@jamesmurdza.com>
2023-05-07 16:18:53 +0200

    Add mkdir correction to echo commands using single quotes.

--- corrections.ts
+++ corrections.ts
@@ -4,15 +4,17 @@ function applyCorrections(buildScript: string) {
 
   // Detect commands like: echo -e "..." > ...
   const echoRedirection = /^((?<COMMAND>echo -e "(?:\\.|[^\\"])*") > (?<DIRECTORY>.*\/).*)$/mg;
+  // Detect commands like: echo -e '...' > ...
+  const echoRedirectionSingleQuote = /^((?<COMMAND>echo -e '(?:\\.|[^\\'])*') > (?<DIRECTORY>.*\/).*)$/mg;
 
   // What a hack! But small corrections like this will necessary for a while.
   return buildScript.replace(/^npx /mg, 'npx --yes ')
     .replace(/^echo( -e)? /mg, 'echo -e ')
     .replace(/^npm install /mg, 'npm install --package-lock-only ')
     .replace(echoRedirection, 'mkdir -p $<DIRECTORY> && $1')
+    .replace(echoRedirectionSingleQuote, 'mkdir -p $<DIRECTORY> && $1')
     .replace(/^git (remote|push|checkout|merge|branch) .*$/mg, '')
     .replace(/^(az|aws|systemctl) .*$/mg, '')
-    .replace(/^git commit -m "(.*)$/mg, 'git commit -m "👷🏼 $1')
     .replace(/^git commit (.*)$/mg, 'git commit -a $1');
 }
 
